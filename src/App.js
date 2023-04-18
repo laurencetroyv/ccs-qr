3deefdae98558fb44f1b73bac1bbd574ef7acb85
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-18 20:08:34 +0800

    feat: added InformationSystems

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import "./App.css";
 
 function App() {
   const Home = lazy(() => import("./routes/Home"));
+  const InformationSystems = lazy(() => import("./routes/InformationSystems"));
 
   return (
     <Suspense>
-      <div className="flex flex-col min-h-screen items-center justify-center">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/IS" element={null} />
-        </Routes>
+      <div data-theme="light">
+        <div className="flex flex-col min-h-screen items-center justify-center">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/IS" element={<InformationSystems />} />
+          </Routes>
+        </div>
       </div>
     </Suspense>
   );
