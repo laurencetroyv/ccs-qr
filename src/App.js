48951bd9c0f2e9f7512ec6a33c78af34c4ca6779
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-18 13:55:35 +0800

    chore: added title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { Fragment } from "react";
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
+
   return (
-    <Fragment>
-      <p>Welcome to React Native</p>
-    </Fragment>
+    <Suspense>
+      <div className="flex min-h-screen">
+        <h1 className="text-center mb-8 sm:text-base md:text-3xl lg:text-5xl font-bold">
+          College of Computer Studies - Student Quick Response Code
+        </h1>
+        <div className="flex grow justify-center items-center">
+          <Routes>
+            <Route path="/" element={null} />
+          </Routes>
+        </div>
+      </div>
+    </Suspense>
   );
 }
 
