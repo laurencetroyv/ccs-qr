c215a62221c098240cfe2561ecc37e8e984c9a43
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-20 01:10:59 +0800

    feat: added all route & remove some styling

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import "./App.css";
 
 function App() {
   const Home = lazy(() => import("./routes/Home"));
-  const InformationSystems = lazy(() => import("./routes/InformationSystems"));
+  const InformationSystems = lazy(() => import("./routes/IS/IS"));
+
+  const Spinner = () => (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="inline-block w-14 h-14 border border-solid rounded-3xl border-t-pink-400 animate-spin" />
+    </div>
+  );
 
   return (
-    <Suspense>
-      <div data-theme="light">
-        <div className="flex flex-col min-h-screen items-center justify-center">
+    <Suspense fallback={<Spinner />}>
+      <div
+        data-theme="dark"
+        className="flex grow items-center justify-center min-h-screen p-4"
+      >
+        <div className="">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/IS" element={<InformationSystems />} />
+            <Route path="IS/*" element={<InformationSystems title={'Information Systems'} />} />
+            <Route path="IT/*" element={null} />
+            <Route path="CA/*" element={null} />
+            <Route path="CS/*" element={null} />
           </Routes>
         </div>
       </div>
