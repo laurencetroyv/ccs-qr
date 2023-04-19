90999bf42aa1dc19daaa9f7d04e1e6b0bf5e4b34
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-19 23:17:40 +0800

    feat: added student provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import StudentProvider from "./utils/StudentProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <StudentProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StudentProvider>
   </React.StrictMode>
 );
