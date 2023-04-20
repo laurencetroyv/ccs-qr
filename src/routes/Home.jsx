f8e53d200d486351d0ecf3abac29569a7053e90f
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-21 02:12:54 +0800

    feat: now uses the students and courses from StudentContext

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,56 +1,71 @@
-import React, { Fragment } from "react";
+import React, { Suspense, useContext } from "react";
 import Card from "../components/Card.jsx";
+import { StudentContext } from "../utils/StudentProvider.jsx";
+import Spinner from "../components/loading-animation.jsx";
 
 export default function Home() {
-  const Major = [
+  const { students, courses } = useContext(StudentContext);
+  const amount = {};
+
+  for (const course in courses) {
+    const enrolledMajor = students.filter(
+      (student) => student.major === courses[course]
+    );
+    amount[`${courses[course]}`] = enrolledMajor.length;
+  }
+
+  const majors = [
     {
       id: 1,
       title: "Computer Application",
       imageURL:
         "https://www.cud.ac.ae/sites/default/files/programs/2020/program-bachelor-of-science-in-computer-science-1920x1080.jpg",
-      alt: "An image of celestial wolf",
-      paragraph: "282 Students Enrolled",
-      route: "CA",
+      student: amount.BSCA,
+      route: "computer-application",
     },
     {
       id: 2,
       title: "Computer Science",
       imageURL:
         "https://th.bing.com/th/id/OIP.hE6i5B7WSwPf8orwRSuTlQHaEK?pid=ImgDet&rs=1",
-      alt: "An image of celestial wolf",
-      paragraph: "158 Students Enrolled",
-      route: "CS",
+      student: amount.BSCS,
+      route: "computer-science",
     },
     {
       id: 3,
       title: "Information Technology",
       imageURL:
         "https://www.asiapacificsecuritymagazine.com/wp-content/uploads/2016/10/how-has-1.jpg",
-      alt: "An image of celestial wolf",
-      paragraph: "304 Students Enrolled",
-      route: "IT",
+      student: amount.BSIT,
+      route: "information-technology",
     },
     {
       id: 4,
       title: "Information Systems",
       imageURL:
         "https://d38b7b1k524qir.cloudfront.net/blog/wp-content/uploads/2019/11/18131839/GettyImages-1176070254-min.jpg",
-      alt: "An image of celestial wolf",
-      paragraph: "127 Students Enrolled",
-      route: "IS",
+      student: amount.BSIS,
+      route: "information-systems",
     },
   ];
 
   return (
-    <Fragment>
-      <h1 className="text-center mb-8 font-bold">
+    <Suspense fallback={<Spinner />}>
+      <h1 className="text-center font-bold sm:text-xl md:text-3xl">
         College of Computer Studies - Student Quick Response Code
       </h1>
-      <div className="flex flex-row space-x-4">
-        {Major.map((data) => (
-          <Card information={data} key={data.id} />
+      <div className="md:flex md:flex-row">
+        {majors.map((data) => (
+          <Card
+            imageURL={data.imageURL}
+            alt={"An image of celestial wolf"}
+            title={data.title}
+            paragraph={`${data.student} students enrolled`}
+            route={data.route}
+            key={data.id}
+          />
         ))}
       </div>
-    </Fragment>
+    </Suspense>
   );
 }
