e1854adfc65c6bfd09be4bd71e17ef07c80f1d2c
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-21 02:19:34 +0800

    chore: added all routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Spinner from "./components/loading-animation";
+import { StudentContext } from "./utils/StudentProvider";
+import InformationSystems from "./routes/information-systems/information-systems";
+import YearComponent from "./routes/year-component/year-component";
 
 function App() {
+  const { fetchStudents } = useContext(StudentContext);
   const Home = lazy(() => import("./routes/Home"));
-  const InformationSystems = lazy(() =>
-    import("./routes/information-systems/information-systems")
-  );
 
+  fetchStudents();
   return (
     <Suspense fallback={<Spinner />}>
       <div
@@ -17,11 +19,85 @@ function App() {
       >
         <div className="">
           <Routes>
+            {/* Information Systems */}
             <Route path="/" element={<Home />} />
-            <Route path="is/*" element={<InformationSystems />} />
-            <Route path="it/*" element={null} />
-            <Route path="ca/*" element={null} />
-            <Route path="cs/*" element={null} />
+            <Route
+              path="information-systems/*"
+              element={<InformationSystems />}
+            />
+            <Route
+              path="information-systems/first-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="information-systems/second-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="information-systems/third-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="information-systems/fourth-year/"
+              element={<YearComponent />}
+            />
+
+            {/* Information Technology */}
+            <Route path="information-technology/" element={null} />
+            <Route
+              path="information-technology/first-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="information-technology/second-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="information-technology/third-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="information-technology/fourth-year/"
+              element={<YearComponent />}
+            />
+
+            {/* Computer Application */}
+            <Route path="computer-application/" element={null} />
+            <Route
+              path="computer-application/first-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="computer-application/second-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="computer-application/third-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="computer-application/fourth-year/"
+              element={<YearComponent />}
+            />
+
+            {/* Computer Science */}
+            <Route path="computer-science/" element={null} />
+            <Route
+              path="computer-science/first-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="computer-science/second-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="computer-science/third-year/"
+              element={<YearComponent />}
+            />
+            <Route
+              path="computer-science/fourth-year/"
+              element={<YearComponent />}
+            />
           </Routes>
         </div>
       </div>
