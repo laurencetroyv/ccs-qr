c13c2f3727646b5513a4280f3751dc4cd11d3573
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-21 02:16:16 +0800

    fix: remove useEffect to fetch students

--- src/utils/StudentProvider.jsx
+++ src/utils/StudentProvider.jsx
@@ -1,13 +1,17 @@
 import { getDocs } from "firebase/firestore";
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import { studentCollection } from "./firebase";
 
 export const StudentContext = createContext();
 
 export default function StudentProvider({ children }) {
   const [students, setStudents] = useState([]);
+  const courses = ["BSIS", "BSIT", "BSCA", "BSCS"];
+  const years = [1, 2, 3, 4];
 
-  useEffect(() => {
+  const yearLevelCount = {};
+
+  const fetchStudents = () => {
     getDocs(studentCollection).then((querySnapshot) => {
       const docs = [];
 
@@ -17,9 +21,25 @@ export default function StudentProvider({ children }) {
 
       setStudents(docs);
     });
-  }, []);
+
+    for (const course of courses) {
+      for (const year of years) {
+        const majorQuery = students.filter(
+          (student) => student.major === courses[course]
+        );
+        const yearQuery = students.filter(
+          (student) => student.year_level === years[year]
+        );
+        yearLevelCount[`${courses[course]} ${years[year]}`] = [
+          majorQuery.length,
+          yearQuery.length,
+        ];
+      }
+    }
+  };
+
   return (
-    <StudentContext.Provider value={{ students }}>
+    <StudentContext.Provider value={{ fetchStudents, students, courses }}>
       {children}
     </StudentContext.Provider>
   );
