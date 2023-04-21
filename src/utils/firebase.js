efcf8fb273d156460d09307af85dab291e8b3d68
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-21 13:50:08 +0800

    chore: firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getFirestore, collection } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId CA optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTJrPC01-T9BVpKXMZCfBidXvB2I5FlxU",
-  authDomain: "msu-ec.firebaseapp.com",
-  projectId: "msu-ec",
-  storageBucket: "msu-ec.appspot.com",
-  messagingSenderId: "235493787272",
-  appId: "1:235493787272:web:f1a57b8b6e34a78668c13d",
-  measurementId: "G-M9H77T8WLD"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
+const studentCollection = collection(db, "students");
+
+export { app, storage, db, studentCollection };
