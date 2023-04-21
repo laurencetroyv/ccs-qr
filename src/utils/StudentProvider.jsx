40efc102c525ebef2796df0f1d2da5f8813a368c
laurencetroyv-gusa <laurencetroyv@gmail.com>
2023-04-21 10:44:28 +0800

    feat: student provider

--- src/utils/StudentProvider.jsx
+++ src/utils/StudentProvider.jsx
@@ -9,8 +9,6 @@ export default function StudentProvider({ children }) {
   const courses = ["BSIS", "BSIT", "BSCA", "BSCS"];
   const years = [1, 2, 3, 4];
 
-  const yearLevelCount = {};
-
   const fetchStudents = () => {
     getDocs(studentCollection).then((querySnapshot) => {
       const docs = [];
@@ -21,25 +19,12 @@ export default function StudentProvider({ children }) {
 
       setStudents(docs);
     });
-
-    for (const course of courses) {
-      for (const year of years) {
-        const majorQuery = students.filter(
-          (student) => student.major === courses[course]
-        );
-        const yearQuery = students.filter(
-          (student) => student.year_level === years[year]
-        );
-        yearLevelCount[`${courses[course]} ${years[year]}`] = [
-          majorQuery.length,
-          yearQuery.length,
-        ];
-      }
-    }
   };
 
   return (
-    <StudentContext.Provider value={{ fetchStudents, students, courses }}>
+    <StudentContext.Provider
+      value={{ fetchStudents, students, courses, years }}
+    >
       {children}
     </StudentContext.Provider>
   );
